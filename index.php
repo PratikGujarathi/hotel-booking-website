8d4e66daf8fc336f4abc300768f4a0e4a112aa2b
PratikGujarathi <pratikgujarathi1818@gmail.com>
2024-07-20 15:38:18 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel Booking</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+ <!-- header section starts -->
+<section class="header">
+    <div class="flex">
+        <a href="#home" class="logo">Hotel And Resorts</a>
+        <a href="#availability" class="btn">Check Availability</a>
+        <div id="menu-btn" class="fas fa-bars"></div>
+        </div>
+    <nav class="navbar">
+        <a href="#home">home</a>
+        <a href="#about">about</a>
+        <a href="#reservation">reservation</a>
+        <a href="#gallery">gallery</a>
+        <a href="#contact">contact</a>
+        <a href="#reviews">reviews</a>
+
+
+    </nav>
+</section>
+  <!-- header section ends -->
+   
+
+  <!-- home section starts -->
+<section class="home" id="home">
+    <div class="home-slider">
+        <div>
+            <div class="box">
+                <img src="images/home-img-1.jpg" alt="">
+                <div class="flex">
+                    <h3>luxurious rooms</h3>
+                    <a href="#availability" class="btn">check availability</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="images/home-img-2.jpg" alt="">
+                <div class="flex">
+                    <h3>foods and drinks</h3>
+                    <a href="#reservation" class="btn">make reservation</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="images/home-img-3.jpg" alt="">
+                <div class="flex">
+                    <h3 class="prtik">luxurious halls</h3>
+                    <a href="#contact" class="btn">contact us</a>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  <!-- home section ends -->
+
+
+
+
+
+
+
+
+<script src="js/script.js"></script>
+</body>
+</html>
